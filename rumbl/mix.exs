e614fe87de02c899a05e4de1690b3c4280673317
Exadra37 <exadra37@gmail.com>
2018-02-23 08:40:34 +0000

    Chapter 5 - Rumbl App - Authenticating users.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- rumbl/mix.exs
+++ rumbl/mix.exs
@@ -5,7 +5,7 @@ defmodule Rumbl.Mixfile do
     [
       app: :rumbl,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
@@ -20,7 +20,16 @@ defmodule Rumbl.Mixfile do
   def application do
     [
       mod: {Rumbl, []},
-      extra_applications: [:logger]
+      extra_applications: [
+        :phoenix,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :postgrex,
+        :comeonin
+      ]
     ]
   end
 
@@ -33,14 +42,15 @@ defmodule Rumbl.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.2.5"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.0"}
     ]
   end
 
