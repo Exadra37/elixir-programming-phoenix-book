bc0d776fede037f443d762774cfe01b2b9baf772
Exadra37 <exadra37@gmail.com>
2018-02-23 20:56:07 +0000

    Chapter 6 - Rumbl App - Generators and Relationships.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -2,6 +2,9 @@ defmodule Rumbl.Auth do
   
   import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+  import Phoenix.Controller
+
+  alias Rumbl.Router.Helpers
 
   # In a Plug init() is only called at compile time.
   def init(opts) do
@@ -40,6 +43,17 @@ defmodule Rumbl.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Helpers.page_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
