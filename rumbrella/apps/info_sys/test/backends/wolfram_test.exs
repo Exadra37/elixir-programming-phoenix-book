8708449020c1d87be451d2fbc50f947d6c1769bd
Exadra37 <exadra37@gmail.com>
2018-03-28 07:44:21 +0000

    --wip-- [skip ci]

--- rumbrella/apps/info_sys/test/backends/wolfram_test.exs
+++ rumbrella/apps/info_sys/test/backends/wolfram_test.exs
@@ -4,19 +4,28 @@ defmodule InfoSys.Backends.WolframTest do
 
   alias InfoSys.Wolfram
 
-  test "makes request, reports results, then terminates" do
+
+  test "makes request, report results, then terminates" do
 
     ref = make_ref()
 
-    # As I ranted on the stub for the Http client, this approach of chaeting
-    # in tests is pure non sense...
-    #
-    # This is not the semantics used to interact with the live system, thus
-    # in my opinion this approach only reveals that the code is not correctly
-    # archtitected, once needs cheating to be tested...
-    {:ok, _} = Wolfram.start_link("1 + 1", ref, self(), 1)
+    {:ok, pid} = Wolfram.start_link("1 + 1", ref, self(), 1)
+
+    Process.monitor(pid)
 
     assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}
+
+    assert_receive {:DOWN, _ref, :process, ^pid, :normal}
+
   end
 
+  test "no query results reports an empty list" do
+
+    ref = make_ref()
+
+    {:ok, _} = Wolfram.start_link("none", ref, self(), 1)
+
+    assert_receive {:results, ^ref, []}
+
+  end
 end
